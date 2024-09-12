bb175d0b1e79f88464adf7e4a7de8313ffdc0f01
Alvin Ramadhani <alvin080297@gmail.com>
2024-09-13 04:57:24 +0700

    update list providers bloc in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,10 @@ import 'package:brik_test/core/common/helper.dart';
 import 'package:brik_test/core/common/navigation.dart';
 import 'package:brik_test/core/common/routes.dart';
 import 'package:brik_test/core/theme/style.dart';
+import 'package:brik_test/feature/groceries/bloc/groceries_add/groceries_add_bloc.dart';
+import 'package:brik_test/feature/groceries/bloc/groceries_update/groceries_update_bloc.dart';
+import 'package:brik_test/feature/groceries/bloc/groceries_delete/groceries_delete_bloc.dart';
+import 'package:brik_test/feature/groceries/bloc/groceries_detail/groceries_detail_bloc.dart';
 import 'package:brik_test/feature/groceries/bloc/groceries_list/groceries_list_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,6 +33,10 @@ class _AppState extends State<App> {
       // list of bloc
       providers: [
         BlocProvider(create: (context) => GroceriesListBloc()),
+        BlocProvider(create: (context) => GroceriesDetailBloc()),
+        BlocProvider(create: (context) => GroceriesAddBloc()),
+        BlocProvider(create: (context) => GroceriesUpdateBloc()),
+        BlocProvider(create: (context) => GroceriesDeleteBloc()),
       ],
       child: MaterialApp(
         navigatorKey: navigatorKey,
